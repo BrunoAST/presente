7b69baffb1a6bbe58cf363779c224f2b5a25bd9b
Bruno Alves <bruno-alves2010@live.com>
2021-06-10 19:43:45 -0300

    Removing unused routing object

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }];
+const routes: Routes = [
+    
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
